d1fd5fd9124a16219779029b4e86e3af7b140ed3
Mathieu Tarral <mathieu.tarral@gmail.com>
2016-03-08 02:34:25 +0100

    emit dbus signals in proccon.cpp

--- src/procexp/proccon/proccon.cpp
+++ src/procexp/proccon/proccon.cpp
@@ -1,5 +1,15 @@
-#include "procconnector.h"
+#include <QtDBus/QDBusConnection>
+#include <QtDBus/QDBusConnectionInterface>
 
+#include "procconnector.h"
+#include "connector.h"
+#include "ConnectorAdapter.h"
+#include "ConnectorInterface.h"
+
+#define CONNECTOR_SERVICE "org.proccon"
+#define CONNECTOR_PATH "/"
+
+static org::proccon* iface = nullptr;
 
 void handler(struct proc_event event)
 {
@@ -7,39 +17,38 @@ void handler(struct proc_event event)
     case event.PROC_EVENT_NONE:
         break;
     case event.PROC_EVENT_FORK:
-        printf("fork: parent tid=%d pid=%d -> child tid=%d pid=%d\n",
+        emit iface->fork(
                event.event_data.fork.parent_pid,
                event.event_data.fork.parent_tgid,
                event.event_data.fork.child_pid,
                event.event_data.fork.child_tgid);
         break;
     case event.PROC_EVENT_EXEC:
-        printf("exec: tid=%d pid=%d\n",
+        emit iface->exec(
                event.event_data.exec.process_pid,
                event.event_data.exec.process_tgid);
         break;
     case event.PROC_EVENT_UID:
-        printf("uid change: tid=%d pid=%d from %d to %d\n",
+        emit iface->uid(
                event.event_data.id.process_pid,
                event.event_data.id.process_tgid,
                event.event_data.id.r.ruid,
                event.event_data.id.e.euid);
         break;
     case event.PROC_EVENT_GID:
-        printf("gid change: tid=%d pid=%d from %d to %d\n",
+        emit iface->gid(
                event.event_data.id.process_pid,
                event.event_data.id.process_tgid,
                event.event_data.id.r.rgid,
                event.event_data.id.e.egid);
         break;
     case event.PROC_EVENT_EXIT:
-        printf("exit: tid=%d pid=%d exit_code=%d\n",
+        emit iface->exit(
                event.event_data.exit.process_pid,
                event.event_data.exit.process_tgid,
                event.event_data.exit.exit_code);
         break;
     default:
-        printf("unhandled proc event\n");
         break;
     }
 
@@ -47,6 +56,19 @@ void handler(struct proc_event event)
 
 int main(int argc, char* argv[])
 {
+    QDBusConnection connection = QDBusConnection::sessionBus();
+
+    Connector* conn = new Connector();
+    ConnectorAdapter* adapter = new ConnectorAdapter(conn);
+
+    if (!connection.registerService(CONNECTOR_SERVICE))
+        qFatal("Could not register the service");
+
+    if (!connection.registerObject(CONNECTOR_PATH, conn))
+        qFatal("Could not register the connector object");
+
+    iface = new org::proccon(CONNECTOR_SERVICE, CONNECTOR_PATH, connection);
+
     ProcConnector connector = ProcConnector();
     connector.addCallback(handler);
     connector.listen();
