03c11259c3d9300e437e8c3d83a4469df4630885
Mathieu Tarral <mathieu.tarral@gmail.com>
2016-03-08 01:45:01 +0100

    add proccon code and dbus interface

---
+++ src/procexp/proccon/connector.h
@@ -0,0 +1,20 @@
+#ifndef CONNECTOR_H
+#define CONNECTOR_H
+
+#include <QObject>
+
+class Connector : public QObject
+{
+    Q_OBJECT
+    Q_CLASSINFO("D-Bus Interface", "org.proccon")
+
+signals:
+    void fork(int parent_pid, int parent_tgid, int child_pid, int child_tgid);
+    void exec(int process_pid, int process_tgid);
+    void uid(int process_pid, int process_tgid, int ruid, int euid);
+    void gid(int process_pid, int process_tgid, int rgid, int egid);
+    void exit(int process_pid, int process_tgid, int exit_code);
+
+};
+
+#endif // CONNECTOR_H
